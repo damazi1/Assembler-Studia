52f94d5c6fc6558a48349570186b8a9bae0a1d38
damazi1 <114773869+damazi1@users.noreply.github.com>
2024-05-22 21:42:26 +0200

    Lista_Lab10 AVX Skończone wszystkie zadania

--- ASMx64/Lab10-AVX_obliczenia/Lab09/main.cpp
+++ ASMx64/Lab10-AVX_obliczenia/Lab09/main.cpp
@@ -16,9 +16,25 @@ extern "C" double asm01_02sd(double, double);
 extern "C" double asm01_03sd(double, double);
 extern "C" double asm01_04sd(double, double);
 extern "C" double asm01_05sd(double, double);
+extern "C" double asm01_06sd(double, double);
 extern "C" double asm01_07sd(double);
+extern "C" float asm01_08ss(float, float, float);
+extern "C" float asm01_09ss(float);
+extern "C" double asm01_11sd(double, double, double, double);
+extern "C" double asm01_12sd(double, double, double, double, double, double);
+extern "C" double asm01_16sd(double);
 extern "C" double asm02_01sd(double, double, double);
+extern "C" double asm02_02sd(double, double, double);
+extern "C" double asm02_03sd(double, double, double);
+extern "C" double asm02_04sd(double, double, double);
+extern "C" float asm02_05ss(float, float);
+extern "C" float asm02_06ss(float, float);
+extern "C" double asm02_07sd(double, double, double, double, double);
 extern "C" double asm02_08sd(double, double, double, double, double);
+extern "C" double asm02_09sd(double, double, double, double, double);
+extern "C" double asm02_10sd(double, double, double, double, double);
+extern "C" double asm02_11sd(double, double, double, double);
+extern "C" double asm02_12sd(double, double, double);
 
 int main() {
 #ifdef _DEBUG
@@ -86,6 +102,18 @@ int main() {
         double yCpp = cpp01_05(a, b);
         resultComparator(yCpp, yAsm);
     }
+    //zad6
+    {
+        setConsoleTextColor(GOLD);
+        cout << "*** 06 ***" << endl;
+        setConsoleTextColor();
+        double a = 0.23, b = 0.59;
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+        double yAsm = asm01_06sd(a, b);
+        double yCpp = cpp01_06(a, b);
+        resultComparator(yCpp, yAsm);
+    }
     //zad7
     {
         setConsoleTextColor(GOLD);
@@ -98,6 +126,82 @@ int main() {
         resultComparator(yCpp, yAsm);
     }
 
+    //zad8
+    {
+        setConsoleTextColor(GOLD);
+        cout << "*** 08 ***" << endl;
+        setConsoleTextColor();
+        float a = rndf<float>();
+        float b = rndf<float>();
+        float x = rndf<float>();
+        cout << "a = " << a << endl;
+        float yAsm = asm01_08ss(a,b,x);
+        float yCpp = cpp01_08(a,b,x);
+        resultComparator(yCpp, yAsm);
+    }
+    //zad9
+    {
+        setConsoleTextColor(GOLD);
+        cout << "*** 09 ***" << endl;
+        setConsoleTextColor();
+        float x = rndf<float>();
+        cout << "x = " << x << endl;
+        float yAsm = asm01_09ss(x);
+        float yCpp = cpp01_09(x);
+        resultComparator(yCpp, yAsm);
+    }
+
+    //zad11
+    {
+        setConsoleTextColor(GOLD);
+        cout << "*** 11 ***" << endl;
+        setConsoleTextColor();
+        double a = rndf<double>();
+        double b = rndf<double>();
+        double c = rndf<double>();
+        double d = rndf<double>();
+        cout << "a = " << a;
+        cout << "\tb = " << b;
+        cout << "\tc = " << c;
+        cout << "\td = " << d << endl;
+        float yAsm = asm01_11sd(a, b, c, d);
+        float yCpp = cpp01_11(a,b,c,d);
+        resultComparator(yCpp, yAsm);
+    }
+
+    //zad12
+    {
+        setConsoleTextColor(GOLD);
+        cout << "*** 12 ***" << endl;
+        setConsoleTextColor();
+        double a = rndf<double>();
+        double b = rndf<double>();
+        double c = rndf<double>();
+        double d = rndf<double>();
+        double e = 0.91;
+        double f = 0.51;
+        cout << "a = " << a;
+        cout << "\tb = " << b;
+        cout << "\tc = " << c;
+        cout << "\td = " << d;
+        cout << "\te = " << e;
+        cout << "\tf = " << f << endl;
+        float yAsm = asm01_12sd(a, b, c, d,e,f);
+        float yCpp = cpp01_12(a, b, c, d,e,f);
+        resultComparator(yCpp, yAsm);
+    }
+    //zad16
+    {
+        setConsoleTextColor(GOLD);
+        cout << "*** 16 ***" << endl;
+        setConsoleTextColor();
+        double r = rndf<double>();
+        cout << "x = " << r << endl;
+        double yAsm = asm01_16sd(r);
+        double yCpp = cpp01_16(r);
+        resultComparator(yCpp, yAsm);
+    }
+    //zad2.1
     {
         setConsoleTextColor(GOLD);
         cout << "*** 2.1 ***" << endl;
@@ -110,7 +214,85 @@ int main() {
         double yCpp = cpp02_01(a, b, x);
         resultComparator(yCpp, yAsm);
     }
-
+    //zad2.2
+    {
+        setConsoleTextColor(GOLD);
+        cout << "*** 2.2 ***" << endl;
+        setConsoleTextColor();
+        double a = rndf<double>(), b = rndf<double>(), x = rndf<double>();
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+        cout << "x = " << x << endl;
+        double yAsm = asm02_02sd(a, b, x);
+        double yCpp = cpp02_02(a, b, x);
+        resultComparator(yCpp, yAsm);
+    }
+    //zad2.3
+    {
+        setConsoleTextColor(GOLD);
+        cout << "*** 2.3 ***" << endl;
+        setConsoleTextColor();
+        double a = rndf<double>(), b = rndf<double>(), x = rndf<double>();
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+        cout << "x = " << x << endl;
+        double yAsm = asm02_03sd(a, b, x);
+        double yCpp = cpp02_03(a, b, x);
+        resultComparator(yCpp, yAsm);
+    }
+    //zad2.4
+    {
+        setConsoleTextColor(GOLD);
+        cout << "*** 2.4 ***" << endl;
+        setConsoleTextColor();
+        double a = rndf<double>(), b = rndf<double>(), x = rndf<double>();
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+        cout << "x = " << x << endl;
+        double yAsm = asm02_04sd(a, b, x);
+        double yCpp = cpp02_04(a, b, x);
+        resultComparator(yCpp, yAsm);
+    }
+    //zad2.5
+    {
+        setConsoleTextColor(GOLD);
+        cout << "*** 2.5 ***" << endl;
+        setConsoleTextColor();
+        float a = rndf<float>(), x = rndf<float>();
+        cout << "a = " << a << endl;
+        cout << "x = " << x << endl;
+        float yAsm = asm02_05ss(a, x);
+        float yCpp = cpp02_05(a, x);
+        resultComparator(yCpp, yAsm);
+    }
+    //zad2.6
+    {
+        setConsoleTextColor(GOLD);
+        cout << "*** 2.6 ***" << endl;
+        setConsoleTextColor();
+        float a = rndf<float>(), x = rndf<float>();
+        cout << "a = " << a << endl;
+        cout << "x = " << x << endl;
+        float yAsm = asm02_06ss(a, x);
+        float yCpp = cpp02_06(a, x);
+        resultComparator(yCpp, yAsm);
+    }
+    //zad2.7
+    {
+        setConsoleTextColor(GOLD);
+        cout << "*** 2.7 ***" << endl;
+        setConsoleTextColor();
+        double a = rndf<double>(), b = rndf<double>(), c = rndf<double>(), d = rndf<double>(), x = rndf<double>();
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+        cout << "c = " << c << endl;
+        cout << "d = " << d << endl;
+        cout << "x = " << x << endl;
+        double yAsm = asm02_07sd(a, b, c, d, x);
+        double yCpp = cpp02_07(a, b, c, d, x);
+        resultComparator(yCpp, yAsm);
+    }
+    //zad2.8
     {
         setConsoleTextColor(GOLD);
         cout << "*** 2.8 ***" << endl;
@@ -123,6 +305,63 @@ int main() {
         cout << "x = " << x << endl;
         double yAsm = asm02_08sd(a, b, c, d, x);
         double yCpp = cpp02_08(a, b, c, d, x);
+        resultComparator(yCpp, yAsm);
+    }
+    //zad2.9
+    {
+        setConsoleTextColor(GOLD);
+        cout << "*** 2.9 ***" << endl;
+        setConsoleTextColor();
+        double a = rndf<double>(), b = rndf<double>(), c = rndf<double>(), d = rndf<double>(), x = rndf<double>();
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+        cout << "c = " << c << endl;
+        cout << "d = " << d << endl;
+        cout << "x = " << x << endl;
+        double yAsm = asm02_09sd(a, b, c, d, x);
+        double yCpp = cpp02_09(a, b, c, d, x);
+        resultComparator(yCpp, yAsm);
+    }
+    //zad2.10
+    {
+        setConsoleTextColor(GOLD);
+        cout << "*** 2.10 ***" << endl;
+        setConsoleTextColor();
+        double a = rndf<double>(), b = rndf<double>(), c = rndf<double>(), d = rndf<double>(), x = rndf<double>();
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+        cout << "c = " << c << endl;
+        cout << "d = " << d << endl;
+        cout << "x = " << x << endl;
+        double yAsm = asm02_10sd(a, b, c, d, x);
+        double yCpp = cpp02_10(a, b, c, d, x);
+        resultComparator(yCpp, yAsm);
+    }
+    //zad2.11
+    {
+        setConsoleTextColor(GOLD);
+        cout << "*** 2.11 ***" << endl;
+        setConsoleTextColor();
+        double a = rndf<double>(), b = rndf<double>(), c = rndf<double>(),  x = rndf<double>();
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+        cout << "c = " << c << endl;
+        cout << "x = " << x << endl;
+        double yAsm = asm02_11sd(a, b, c, x);
+        double yCpp = cpp02_11(a, b, c, x);
+        resultComparator(yCpp, yAsm);
+    }
+    //zad2.12
+    {
+        setConsoleTextColor(GOLD);
+        cout << "*** 2.12 ***" << endl;
+        setConsoleTextColor();
+        double a = rndf<double>(), b = rndf<double>(), x = rndf<double>();
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+        cout << "x = " << x << endl;
+        double yAsm = asm02_12sd(a, b, x);
+        double yCpp = cpp02_12(a, b, x);
         resultComparator(yCpp, yAsm);
     }
     system("pause");
--- ASMx64/Lab10-AVX_obliczenia/Lab09/source.asm
+++ ASMx64/Lab10-AVX_obliczenia/Lab09/source.asm
@@ -1,8 +1,17 @@
 .data
+sign_mask_ss DD 7FFFFFFFh
 sign_mask_sd DQ 7FFFFFFFFFFFFFFFh
 PI           DQ 3.14159265358979323846
 _l6          DQ 6.0
 _l10         DQ 10.0
+_l1          DQ 1.0
+_l2          DQ 2.0
+_l4          DQ 4.0
+_l3          DQ 3.0
+_15          DD 15.0
+_5           DD 5.0
+_6           DD 6.0
+_2           DD 2.0
 .code
 
 ; xmm0 <- double a
@@ -42,11 +51,87 @@ local _tmp:qword
     ret
 asm01_05sd ENDP
 
+; xmm0 <- double a
+; xmm1 <- double b;
+; return double sqrt(a+b) -> xmm0
+asm01_06sd PROC
+    vaddsd xmm0, xmm0, xmm1; a+b
+    vsqrtsd xmm0, xmm0, xmm0; sqrt(a+b)
+    ret
+asm01_06sd ENDP
+
 ;xmm0 <- double a 
 asm01_07sd PROC
     vandpd xmm0, xmm0, sign_mask_sd
     ret
 asm01_07sd ENDP
+
+; xmm0 <- float a
+; xmm1 <- float b
+; xmm2 <- float x
+asm01_08ss PROC
+    vmulss xmm3, xmm2, xmm2; xmm3 <- x*x
+    vmulss xmm2, xmm3, xmm2; xmm2 <- x*x*x
+    vmulss xmm0, xmm0, xmm2; xmm0 <- ax^3
+    vaddss xmm0, xmm0, xmm1; xmm0 <- ax^3+b
+    vandps xmm0, xmm0, sign_mask_ss; xmm0 <- |ax^3+b|
+    ret
+asm01_08ss ENDP
+
+; xmm0 <- float x
+asm01_09ss PROC
+local _tmp:dword
+    vmulss xmm1, xmm0, xmm0; xmm1 <- x*x
+    vmulss xmm2, xmm1, xmm0; xmm2 <- x*x*x
+    vmulss xmm2, xmm2, _15; xmm2 <- 15x^3
+    vmulss xmm1, xmm1, _5; xmm1 <- 5x^2
+    vmulss xmm0, xmm0, _6; xmm0 <- 6x
+    vaddss xmm2, xmm2, xmm1; xmm2 <- 15x^3 + 5x^2
+    vsubss xmm0, xmm2, xmm0; xmm0 <- 15x^3 + 5x^2 - 6x
+    vaddss xmm0, xmm0, _2; xmm0 <- wynik
+    ret
+asm01_09ss ENDP
+
+; xmm0 <- double a
+; xmm1 <- double b
+; xmm2 <- double c
+; xmm3 <- double d
+asm01_11sd PROC
+    vaddsd xmm0, xmm0, xmm1; xmm0 <- a+b
+    vmulsd xmm0, xmm0, xmm0; xmm0 <- (a+b)^2
+    vmulsd xmm2, xmm2, xmm3; xmm2 <- cd
+    vaddsd xmm2, xmm2, _l1; xmm2 <- cd+1
+    vsqrtsd xmm2,xmm2,xmm2; xmm2 <- sqrt(content)
+    vsubsd xmm0, xmm0, xmm2; xmm0 <- wynik
+    ret
+asm01_11sd ENDP
+
+; xmm0 <- double a
+; xmm1 <- double b
+; xmm2 <- double c
+; xmm3 <- double d
+; stos <- double e
+; stos <- double f
+asm01_12sd PROC a:qword, b:qword, c:qword, d:qword, e:qword, f:qword
+    vmulsd xmm0, xmm0, xmm1; xmm0 <- ab
+    vmulsd xmm1, xmm2, xmm3; xmm2 <- cd
+    vaddsd xmm0, xmm0, xmm1 ; xmm0 <- ab+cd
+    movsd xmm1, e
+    vmulsd xmm1, xmm1, f; xmm1 <- ef
+    vsqrtsd xmm1, xmm1, xmm1; xmm2 <- sqrt(ef)  
+    vdivsd xmm0, xmm0, xmm1; xmm0 <- ab+cd / sqrt(ef)
+    ret
+asm01_12sd ENDP
+
+; xmm0 <- double r
+asm01_16sd PROC
+    vmulsd xmm1, xmm0, xmm0
+    vmulsd xmm0, xmm1, xmm0; xmm0 <-r^3
+    vmulsd xmm0, xmm0, PI; xmm0 <- PIr^3
+    vmulsd xmm0, xmm0, _l4;
+    vdivsd xmm0, xmm0, _l3;
+    ret
+asm01_16sd ENDP
 
 ; xmm0 <- a
 ; xmm1 <- b
@@ -55,6 +140,58 @@ asm02_01sd PROC
     vfmadd132sd xmm0, xmm1, xmm2 
     ret
 asm02_01sd ENDP 
+
+; xmm0 <- double a
+; xmm1 <- double b
+; xmm2 <- double x
+asm02_02sd PROC
+    vfmsub132sd xmm0, xmm1, xmm2
+    ret
+asm02_02sd ENDP
+
+; xmm0 <- double a
+; xmm1 <- double b
+; xmm2 <- double x
+asm02_03sd PROC
+    vfnmadd132sd xmm0, xmm1, xmm2
+    ret
+asm02_03sd ENDP
+
+; xmm0 <- double a
+; xmm1 <- double b
+; xmm2 <- double x
+asm02_04sd PROC
+    vfnmsub132sd xmm0, xmm1, xmm2
+    ret
+asm02_04sd ENDP
+
+; xmm0 <- float a
+; xmm1 <- float x
+asm02_05ss PROC
+    vfmadd231ss xmm0, xmm1, xmm1
+    ret
+asm02_05ss ENDP
+
+; xmm0 <- float a
+; xmm1 <- float x
+asm02_06ss PROC
+    vfmsub231ss xmm0, xmm1, xmm1
+    ret
+asm02_06ss ENDP
+
+; xmm0 <- double a
+; xmm1 <- double b
+; xmm2 <- double c
+; xmm3 <- double d
+; stos <- double x
+; return x(x(ax+b)+c)+d
+asm02_07sd PROC a:qword, b:qword, c:qword, d:qword, x:qword
+    movsd xmm4, x
+    vfmadd132sd xmm0, xmm1, xmm4; xmm0 <- ax+b
+    vfmadd132sd xmm0, xmm2, xmm4; xmm0 <- x(ax+b)
+    vfmadd132sd xmm0, xmm3, xmm4; xmm0 <- wynik
+    ret
+asm02_07sd ENDP
 
 ; xmm0 <- a
 ; xmm1 <- b
@@ -71,4 +208,57 @@ asm02_08sd PROC a:qword, b:qword, c:qword, d:qword, x:qword
     vfmsub132sd xmm0, xmm3, x ; xmm0 <- xww-d
     ret
 asm02_08sd ENDP
+
+; xmm0 <- double a
+; xmm1 <- double b
+; xmm2 <- double c
+; xmm3 <- double d
+; stos <- double x
+; return x(x(-ax+b)+c)+d
+asm02_09sd PROC a:qword, b:qword, c:qword, d:qword, x:qword
+    movsd xmm4, x
+    vfnmadd132sd xmm0, xmm1, xmm4; xmm0 <- -ax+b
+    vfmadd132sd xmm0, xmm2, xmm4; xmm0 <- x(-ax+b)+c
+    vfmadd132sd xmm0, xmm3, xmm4; xmm0 <- wynik;
+    ret
+asm02_09sd ENDP
+
+; xmm0 <- double a
+; xmm1 <- double b
+; xmm2 <- double c
+; xmm3 <- double d
+; stos <- double x
+; return x(x(-ax+b)+c)-d
+asm02_10sd PROC a:qword, b:qword, c:qword, d:qword, x:qword
+    movsd xmm4, x
+    vfnmadd132sd xmm0, xmm1, xmm4
+    vfmadd132sd xmm0, xmm2, xmm4
+    vfmsub132sd xmm0, xmm3, xmm4
+    ret
+asm02_10sd ENDP
+
+; xmm0 <- double a
+; xmm1 <- double b
+; xmm2 <- double c
+; xmm3 <- double x
+; return ax^2+bc
+asm02_11sd PROC
+    vmulsd xmm3, xmm3, xmm3; x^2
+    vmulsd xmm0, xmm0, xmm3; ax^2
+    vfmadd231sd xmm0, xmm1, xmm2; xmm0 <- wynik
+    ret
+asm02_11sd ENDP
+
+; xmm0 <- double a
+; xmm1 <- double b
+; xmm2 <- double x
+; return x(a+2b+x)+b^2
+asm02_12sd PROC
+    vmulsd xmm3, xmm1, _l2; xmm3  <- 2b
+    vaddsd xmm0, xmm0, xmm3; xmm0 <- a+2b
+    vaddsd xmm0, xmm0, xmm2; xmm0 <- a+2b+x
+    vmulsd xmm1, xmm1, xmm1; xmm1 <- b*b
+    vfmadd132sd xmm0, xmm1, xmm2; xmm0 <- wynik
+    ret
+asm02_12sd ENDP
 END
